00ddb6649c4f36825e0693860ff1305dd7645c80
Mohammad Liton Hossain <litu702@yahoo.com>
2021-07-08 22:03:11 +0600

    Add files via upload

--- movies.sql
+++ movies.sql
@@ -43,9 +43,37 @@ where rownum<=10
 
 ------------------------------------------------------------------
 --correlation between numerical columns
+select round(corr(votes,gross),2)
+from movies 
 select round(corr(budget,gross),2)
 from movies 
 ----------------------------------------------------------
 --count the movie types
 select genre,count(*) as Num_of_movies from movies group by genre order by Num_of_movies desc 
-
+------------------------------------------------------------------------
+--looking for the yearly top 5 types of movies by means of revenue
+select *from (
+select genre,sum(budget) as budget_total,sum(gross) as total_income from movies 
+group by genre
+order by total_income desc)
+where rownum<=5
+-------------------------------------------------------------
+--looking for the top 5 types of movies by means of revenue along with net income ratio 
+select *from (
+select genre,sum(budget) as budget_total,sum(gross) as total_income, 
+sum(gross)-sum(budget) as net_income, 
+round(((sum(gross)-sum(budget))/sum(budget))*100,2) as income_ratio, sum(votes) as total_votes from movies 
+having sum(budget)>0--we have zero budget in war types movie
+group by genre
+order by income_ratio desc)
+where rownum<=5
+--------------------------------------------------------------------------------------------------
+--looking for the yearly top 5 types of movies by means of revenue along with net income ratio 
+select *from (
+select year,genre,sum(budget) as budget_total,sum(gross) as total_income, 
+sum(gross)-sum(budget) as net_income, 
+round(((sum(gross)-sum(budget))/sum(budget))*100,2) as income_ratio, sum(votes) as total_votes from movies 
+having sum(budget)>0--we have zero budget in war types movie
+group by year,genre
+order by income_ratio desc)
+where rownum<=5
